f758afe9ad42cf7d3b405768e5280a413fa562f1
cdaley95 <10672938@uvu.edu>
2024-05-20 12:21:49 -0600

    Update main.py
    Testing main in github

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+'''
+Project Name: UVSim
+Contributors: Corey Daley, Josh Keyes, Luke Raven, Zachary Wilson
+Course: CS2450
+Group: J
+'''
 
+def hello():
+  print("Hello World!")
+  return
+
+def main():
+  hello()
+  return
+
+if __name__ == "__main__":
+  main()
