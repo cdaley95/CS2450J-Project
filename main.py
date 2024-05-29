3769a5e329f2d07ec9d31e1280c1d39b0876b10b
cdaley95 <10672938@uvu.edu>
2024-05-29 12:14:59 -0600

    Load, Store methods and basic init

--- main.py
+++ main.py
@@ -5,14 +5,36 @@ Course: CS2450
 Group: J
 '''
 
-def hello():
-  print("Hello World! TEST NEW FEATURE")
-  print("testing a feature specific 2")
-  return
+class BasicML:
+    '''Initialized with a 100 word memory and a single
+    register named accumulator, as well as a dictionary
+    for the opcodes'''
+    def __init__(self):
+        self.memory = ["+0000"]*100
+        self.accumulator = "+0000"
+        self.pointer = 0
+
+        #Dictionary mapping opcodes to class methods
+        self.instructions = {
+            20: self.load,
+            21: self.store
+        }
+
+    def load(self):
+        'Loads word from memory address into accumulator'
+        self.accumulator = self.memory[int(self.memory[self.pointer][3:])]
+
+    def store(self):
+        'Stores word from accumulator into memory address'
+        self.memory[int(self.memory[self.pointer][3:])] = self.accumulator
+
+    #Needs a class method to run the program and iterate the pointer
+
 
 def main():
-  hello()
-  return
+    '''main method docstring'''
+    #need to implement loading from file here
+    return
 
 if __name__ == "__main__":
-  main()
+    main()
