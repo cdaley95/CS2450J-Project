196dd1102e4b50afefa69c3312b862dab1cfd982
cdaley95 <10672938@uvu.edu>
2024-06-07 12:10:47 -0600

    Update test_main.py
    Fix load and store tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -135,33 +135,26 @@ def test_divide():
     assert ml.accumulator == "-0001"
 
 
-def test_load_1():
-    loadtest1 = BasicML()
-    loadtest1.memory[0:3] = ["+2002", "+1054", "-9756"]
-    loadtest1.load()
-    assert loadtest1.accumulator == "-9756"
+def test_load():
+    loadtest = BasicML()
+    loadtest.memory[0:3] = ["+2002", "+1054", "-9756"]
+    address = 0
+    loadtest.load(address)
+    assert loadtest.accumulator == "-9756"
+    loadtest.memory[66:69] = ["-2068", "+1010", "+8888"]
+    address = 66
+    loadtest.load(address)
+    assert loadtest.accumulator == "+8888"
 
-
-def test_load_2():
-    loadtest2 = BasicML()
-    loadtest2.memory[66:69] = ["-2068", "+1010", "+8888"]
-    loadtest2.pointer = 66
-    loadtest2.load()
-    assert loadtest2.accumulator == "+8888"
-
-
-def test_store_1():
-    storetest1 = BasicML()
-    storetest1.memory[0:2] = ["-2102", "+1030"]
-    storetest1.accumulator = "-9645"
-    storetest1.store()
-    assert storetest1.memory[2] == "-9645"
-
-
-def test_store_2():
-    storetest2 = BasicML()
-    storetest2.memory[89:92] = ["+2169", "+1030", "-5674"]
-    storetest2.accumulator = "-7532"
-    storetest2.pointer = 89
-    storetest2.store()
-    assert storetest2.memory[69] == "-7532"
+def test_store():
+    storetest = BasicML()
+    storetest.memory[0:2] = ["-2102", "+1030"]
+    address = 0
+    storetest.accumulator = "-9645"
+    storetest1.store(address)
+    assert storetest.memory[2] == "-9645"
+    storetest.memory[89:92] = ["+2169", "+1030", "-5674"]
+    storetest.accumulator = "-7532"
+    address = 89
+    storetest.store(address)
+    assert storetest.memory[69] == "-7532"
