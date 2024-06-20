6a9827839cc4102b8e7290e731c62fbaf399bf96
cdaley95 <10672938@uvu.edu>
2024-06-20 16:00:14 -0600

    Update main.py
    Implemented lambda function so reset pointer and accumulator method is not modified.

--- src/main.py
+++ src/main.py
@@ -223,7 +223,8 @@ class BasicMLGUI():
                                        text="Update", command=self.update_both_entry)
         self.update_button.pack(side=tk.LEFT, padx=5)
         self.reset_button = tk.Button(self.control_frame,
-                                       text="Reset", command=self.reset_pointer_accumulator(True))
+                                       text="Reset", command=lambda:(self.reset_pointer_accumulator(),
+                                                                    self.clear_console()))
         self.reset_button.pack(side=tk.LEFT, padx=5)
 
     def buttons(self):
@@ -309,15 +310,10 @@ class BasicMLGUI():
         self.accumulator_entry.delete(0, tk.END)
         self.accumulator_entry.insert(0, f"{self.ml.accumulator}")
 
-    def reset_pointer_accumulator(self, clear_console=False):
+    def reset_pointer_accumulator(self):
         '''Reset pointer and accumulator to initial values'''
         self.reset_accumulator()
         self.reset_pointer()
-        try:
-            if clear_console is True:
-                self.clear_console()
-        except AttributeError:
-            pass
 
     def run_program(self):
         '''runs program by iterating step program'''
